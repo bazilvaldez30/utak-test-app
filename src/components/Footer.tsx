50cded62a79479a135ce15fd83564096af725348
John Bazil Milan Valdez <bazilvaldez30@gmail.com>
2024-06-11 15:07:51 +0800

    fix build error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export default function Footer() {
   return (
     <footer className='mb-10 px-4 text-center text-gray-500 mt-20 flex flex-col justify-center items-center'>
